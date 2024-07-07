e5fbd789a61981292dfc747bf5eb91ce17f72d3d
Nicko Corriveau <ncorriveau13@gmail.com>
2024-07-07 21:46:18 +0000

    add 4 way loop unrolling

--- kernels/starter_code/loop_unrolling.cc
+++ kernels/starter_code/loop_unrolling.cc
@@ -29,10 +29,10 @@ void MatmulOperator::mat_mul_loop_unrolling(struct matmul_params *params) {
                 // pointer of the int8 activation
                 const signed char *a_int8 = &A->int8_data_ptr[row * k + ch];
                 // pointer of the int4 weights
-                uint8_t *w0_int4 = &B->int4_data_ptr[(col * k + ch) / 2];
-                uint8_t *w1_int4 = &B->int4_data_ptr[((col + 1) * k + ch) / 2];
-                uint8_t *w2_int4 = &B->int4_data_ptr[((col + 2) * k + ch) / 2];
-                uint8_t *w3_int4 = &B->int4_data_ptr[((col + 3) * k + ch) / 2];
+                uint8_t* w0_int4 = &B->int4_data_ptr[(col * k + ch) / 2];
+                uint8_t* w1_int4 = &B->int4_data_ptr[((col + 1) * k + ch) / 2];
+                uint8_t* w2_int4 = &B->int4_data_ptr[((col + 2) * k + ch) / 2];
+                uint8_t* w3_int4 = &B->int4_data_ptr[((col + 3) * k + ch) / 2];
                 // scale of activation
                 float s_a = params->A_scales[(row * k + ch) / block_size];
                 // scale of weight
@@ -55,8 +55,6 @@ void MatmulOperator::mat_mul_loop_unrolling(struct matmul_params *params) {
                 int intermediate_sum0 = 0, intermediate_sum1 = 0, intermediate_sum2 = 0, intermediate_sum3 = 0;
                 for (int qj = 0; qj < 16; qj++) {
                     // TODO: decode a packed byte into two int8 in the range of (-8, 7)
-
-                    // TODO: int8 multiply and accumulate operation
                 }
                 // dequantize the sum into floating point
                 acc0 += (float)intermediate_sum0 * s_a * s_w0;
@@ -74,11 +72,11 @@ void MatmulOperator::mat_mul_loop_unrolling(struct matmul_params *params) {
                 float s_a_2nd = params->A_scales[(row * k + ch) / block_size + 1];
                 // order of weights with QM_x86:
                 // origin order: (w0,w1), (w2,w3), (w4,w5), (w6,w7), (w8, w9), ... (w62,w63)
-                // QM_ARM order: (w0,w32),(w1,w33),(w2,w34),(w3,w35),(w4, w36),... (w31,w63)
+                // QM_x86 order: (w0,w32),(w1,w33),(w2,w34),(w3,w35),(w4, w36),... (w31,w63)
                 //               |--|
                 //               4 bits
                 //               |------|
-                //               8 bits (byte)
+                //               8 bits (byte)P
                 //            low|----------------------------------------------------------|high
                 //               0                         256 bit
                 // process 32 bytes of weigths (256 bit) = 2 blocks for each of unrolled `col`
@@ -88,10 +86,39 @@ void MatmulOperator::mat_mul_loop_unrolling(struct matmul_params *params) {
                     intermediate_sum3_2nd = 0;
                 for (int qj = 0; qj < 32; qj++) {
                     // TODO: decode a packed byte into two int8 in the range of (-8, 7)
+                    uint8_t packed_int4_0 = w0_int4[qj];
+                    uint8_t packed_int4_1 = w1_int4[qj];
+                    uint8_t packed_int4_2 = w2_int4[qj];
+                    uint8_t packed_int4_3 = w3_int4[qj];
+
+                    signed char w_de_0_0 = (packed_int4_0 & 0x0F) - 8.0;  
+                    signed char w_de_16_0 = (packed_int4_0 >> 4) - 8.0;   
+                    
+                    signed char w_de_0_1 = (packed_int4_1 & 0x0F) - 8.0;
+                    signed char w_de_16_1 = (packed_int4_1 >> 4) - 8.0;
+                    
+                    signed char w_de_0_2 = (packed_int4_2 & 0x0F) - 8.0;
+                    signed char w_de_16_2 = (packed_int4_2 >> 4) - 8.0;
+                    
+                    signed char w_de_0_3 = (packed_int4_3 & 0x0F) - 8.0;
+                    signed char w_de_16_3 = (packed_int4_3 >> 4) - 8.0;
 
                     // TODO: int8 multiply and accumulate operation
+                    intermediate_sum0 += a_int8[qj] * w_de_0_0;
+                    intermediate_sum0_2nd += a_int8[qj + 32] * w_de_16_0;
+                    
+                    intermediate_sum1 += a_int8[qj] * w_de_0_1;
+                    intermediate_sum1_2nd += a_int8[qj+ 32] * w_de_16_1;
+                    
+                    intermediate_sum2 += a_int8[qj] * w_de_0_2;
+                    intermediate_sum2_2nd += a_int8[qj + 32] * w_de_16_2;
+                    
+                    intermediate_sum3 += a_int8[qj] * w_de_0_3;
+                    intermediate_sum3_2nd += a_int8[qj + 32] * w_de_16_3;
+
                 }
                 // dequantize the sum into floating point
+                // we only have one s_a and are only going thru the weight matrix for unrolling 
                 acc0 += (float)intermediate_sum0 * s_a * s_w0;
                 acc0 += (float)intermediate_sum0_2nd * s_a_2nd * s_w0_2nd;
                 acc1 += (float)intermediate_sum1 * s_a * s_w1;
